339ede75478ca7cb27d183e6715b12cdbecb9f88
oyilapampa-lang <oyilapampa@gmail.com>
2025-09-25 19:01:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Esto es un comentario en JavaScript.
+// Este código se ejecutará tan pronto como la página se haya cargado completamente.
+
+window.onload = function() {
+    // Definimos el texto del mensaje emergente:
+    var mensaje = "¡AGENCIA 128 te da la bienvenida! ¡Estamos listos para tus proyectos web!";
+    
+    // La función 'alert()' crea la ventana emergente en el navegador:
+    alert(mensaje);
+};
